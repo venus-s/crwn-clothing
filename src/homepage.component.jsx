79885f43f46917fed063eba8512decaddab0025e
Venus Soontornprueksa <venus.soontornprueksa@gmail.com>
2021-06-26 11:00:42 -0700

    Create project with create react app and add homepage files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import './homepage.styles.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">hi</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
