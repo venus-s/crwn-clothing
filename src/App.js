8c39e52dd8264d390263376ed3f03dc5910b5a6b
vsoontornprueksa <vsoontornprueksa@aurasolutionsllc.com>
2021-09-23 12:54:09 -0700

    Retrieve shop data from firestore, add spinner to pages when shop data is loading

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
 
